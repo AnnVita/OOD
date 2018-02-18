121389f19d4707be7c3d0142ba7b5cc2dd586f6c
Anna Kublitskaya <kublitskayavd@yandex.ru>
2018-02-18 16:57:44 +0300

    lab 01
    lab 01 is fully completed

--- lab01/strategy/fp-strategy/main.cpp
+++ lab01/strategy/fp-strategy/main.cpp
@@ -1,15 +1,204 @@
-﻿#include <algorithm>
+﻿#include <iostream>
 #include <functional>
 
 using namespace std;
 
+void DanceWaltzBehavior()
+{
+	cout << "I'm dancing waltz ^_^" << endl;
+}
+
+void DanceMinuetBehavior()
+{
+	cout << "I'm dancing minuet ^_^" << endl;
+}
+
+void NoDanceBehavior()
+{
+}
+
+void QuackBehavior()
+{
+	cout << "Quack Quack!!!" << endl;
+}
+
+void SqueakBehavior()
+{
+	cout << "Squeek!!!" << endl;
+}
+
+void MuteQuackBehavior()
+{
+}
+
+void FlyNoWayBehavior()
+{
+}
+
+function<void()> FlyWithWings()
+{
+	return[flightCounter = 0]() mutable
+	{
+		cout << "I'm flying with wings! My flight counter is " << ++flightCounter << " now." << endl;
+	};
+}
+
+class Duck
+{
+public:
+	Duck(function<void()> flyBehavior,
+		function<void()> quackBehavior,
+		function<void()> danceBehavior)
+		: m_quackBehavior(quackBehavior)
+		, m_danceBehavior(danceBehavior)
+	{
+		SetFlyBehavior(flyBehavior);
+	}
+
+	void Quack()
+	{
+		m_quackBehavior();
+	}
+
+	void Swim()
+	{
+		cout << "I'm swimming" << endl;
+	}
+
+	void Fly()
+	{
+		m_flyBehavior();
+	}
+
+	void Dance()
+	{
+		m_danceBehavior();
+	}
+
+	void SetFlyBehavior(function<void()> flyBehavior)
+	{
+		m_flyBehavior = flyBehavior;
+	}
+
+	virtual void Display() const = 0;
+	virtual ~Duck() = default;
+
+private:
+	function<void()> m_flyBehavior;
+	function<void()> m_quackBehavior;
+	function<void()> m_danceBehavior;
+};
+
+class MallardDuck : public Duck
+{
+public:
+	MallardDuck()
+		: Duck(FlyWithWings(),
+			QuackBehavior,
+			DanceWaltzBehavior)
+	{
+	}
+
+	void Display() const override
+	{
+		cout << "I'm mallard duck" << endl;
+	}
+};
+
+class RedheadDuck : public Duck
+{
+public:
+	RedheadDuck()
+		: Duck(FlyWithWings(),
+			QuackBehavior,
+			DanceMinuetBehavior)
+	{
+	}
+	void Display() const override
+	{
+		cout << "I'm redhead duck" << endl;
+	}
+};
+
+class DeckoyDuck : public Duck
+{
+public:
+	DeckoyDuck()
+		: Duck(FlyNoWayBehavior,
+			MuteQuackBehavior,
+			NoDanceBehavior)
+	{
+	}
+
+	void Display() const override
+	{
+		cout << "I'm deckoy duck" << endl;
+	}
+};
+
+class RubberDuck : public Duck
+{
+public:
+	RubberDuck()
+		: Duck(FlyNoWayBehavior,
+			SqueakBehavior,
+			NoDanceBehavior)
+	{
+	}
+	void Display() const override
+	{
+		cout << "I'm rubber duck" << endl;
+	}
+};
+
+class ModelDuck : public Duck
+{
+public:
+	ModelDuck()
+		: Duck(FlyNoWayBehavior,
+			QuackBehavior,
+			NoDanceBehavior)
+	{
+	}
+
+	void Display() const override
+	{
+		cout << "I'm model duck" << endl;
+	}
+};
+
+void DrawDuck(Duck const& duck)
+{
+	duck.Display();
+}
+
+void PlayWithDuck(Duck & duck)
+{
+	DrawDuck(duck);
+	duck.Swim();
+	duck.Quack();
+	duck.Fly();
+	duck.Dance();
+	cout << endl;
+}
+
 void main()
 {
-	int numbers[] = {6, 3, 2, 1, 3, 4};
-	sort(begin(numbers), end(numbers), [](int x, int y) { return x < y; });
-	
-	sort(begin(numbers), end(numbers), less<int>());
+	MallardDuck mallarDuck;
+	PlayWithDuck(mallarDuck);
 
-	function<bool(int, int)> compare = [](int x, int y) { return x < y; };
-	sort(begin(numbers), end(numbers), compare);
+	RedheadDuck redheadDuck;
+	PlayWithDuck(redheadDuck);
+
+	RubberDuck rubberDuck;
+	PlayWithDuck(rubberDuck);
+
+	DeckoyDuck deckoyDuck;
+	PlayWithDuck(deckoyDuck);
+
+	ModelDuck modelDuck;
+	PlayWithDuck(modelDuck);
+	modelDuck.SetFlyBehavior(FlyWithWings());
+	PlayWithDuck(modelDuck);
+	modelDuck.Fly();
 }
