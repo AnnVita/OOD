1c9d7742c231b76e0896ada3c586b79d7b83c51d
Anna Kublitskaya <kublitskayavd@yandex.ru>
2018-05-31 13:15:26 +0300

    lab05

---
+++ lab05/command/TextEditor/TextEditorTests/TextEditorTests.cpp
@@ -0,0 +1,24 @@
+#include "stdafx.h"
+#include "../TextEditor/CDocument.h"
+
+using namespace std;
+
+struct Document_
+{
+	CDocument document;
+};
+
+BOOST_FIXTURE_TEST_SUITE(Document, Document_)
+BOOST_AUTO_TEST_SUITE(when_created)
+BOOST_AUTO_TEST_CASE(have_no_title)
+{
+	BOOST_CHECK_EQUAL(document.GetTitle(), "");
+}
+
+BOOST_AUTO_TEST_CASE(can_have_new_title)
+{
+	document.SetTitle("Test title");
+	BOOST_CHECK_EQUAL("Test title", document.GetTitle());
+}
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
---
+++ lab05/command/TextEditor/TextEditorTests/stdafx.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+#include <sstream>
+#include <memory>
+
+#define BOOST_TEST_INCLUDED
+#pragma warning (push, 3)
+#pragma warning (disable:4456 4702)
+#include <boost/test/unit_test.hpp>
+#pragma warning (pop)
+
+#include <boost/range/algorithm/copy.hpp>
+#include <boost/algorithm/cxx11/all_of.hpp>
